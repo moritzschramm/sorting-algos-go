6e9d744e6dcf6766231e80e21f6d4cb91b6711eb
Moritz Schramm <moritz.schramm1@gmail.com>
2018-04-21 20:59:58 +0200

    create package generator (to generate array with random values)

---
+++ generator/generator.go
@@ -0,0 +1,17 @@
+package generator
+
+import (
+	"time"
+	"math/rand"
+)
+
+func Generate(length, maxRand int) []int {
+
+	rand.Seed(time.Now().UnixNano())
+	a := make([]int, length)
+
+	for i := 0; i < length; i++ {			// initialize slice with random values
+		a[i] = rand.Intn(maxRand)
+	}
+	return a
+}
